5efa20dacfc2722bc7b09416b373b8dee9aa971e
palinkas-jo-reggelt <52140617+palinkas-jo-reggelt@users.noreply.github.com>
2023-07-01 18:04:36 -0400

    v9
    housekeeping; added radius equal to 'accuracy_radius' from database

--- functions.php
+++ functions.php
@@ -7,8 +7,11 @@
 
 	function isPrivateOrLoopback($geoip){
 		if (preg_match("/^(::1|fc|fd|fe|ff|100)/",$geoip)) {return true;}
-		else if (preg_match("/^(10\.|127\.|169\.254|192\.0\.0\.|192\.0\.2\.|192\.88\.99|192\.168|198\.1(8|9)\.|198\.51\.100|203\.0\.113|233\.252\.0|255.255.255.255)/",$geoip)) {return true;}
+		else if (preg_match("/^(0\.|10\.|127\.|169\.254|192\.0\.0\.|192\.0\.2\.|192\.88\.99|192\.168|198\.1(8|9)\.|198\.51\.100|203\.0\.113|233\.252\.0)/",$geoip)) {return true;}
 		else if ((ip2long($geoip) >= ip2long('100.64.0.0')) && (ip2long($geoip) <= ip2long('100.127.255.255'))) {return true;}
+		else if ((ip2long($geoip) >= ip2long('172.16.0.0')) && (ip2long($geoip) <= ip2long('172.31.255.255'))) {return true;}
+		else if ((ip2long($geoip) >= ip2long('224.0.0.0')) && (ip2long($geoip) <= ip2long('239.255.255.255'))) {return true;}
+		else if ((ip2long($geoip) >= ip2long('240.0.0.0')) && (ip2long($geoip) <= ip2long('255.255.255.255'))) {return true;}
 		else {return false;}
 	}
 
--- index.php
+++ index.php
@@ -66,66 +66,87 @@
 			$longitude = $row['longitude'];
 			if (strlen($row['accuracy_radius'])>0) {$km = " km";} else {$km = "&nbsp;";}
 			if ($row['is_in_european_union']==0) {$eu = "false";} else {$eu = "true";}
+			$accuracy_radius = $row['accuracy_radius'];
 			echo "
 						<div class='simple-div-table-row'>
-							<div class='simple-div-table-col left'>ip</a>&nbsp;</div><div class='simple-div-table-col right'>".$geoip."&nbsp;</div>
+							<div class='simple-div-table-col left'>ip</a>&nbsp;</div>
+							<div class='simple-div-table-col right'>".$geoip."&nbsp;</div>
 						</div>
 						<div class='simple-div-table-row'>
-							<div class='simple-div-table-col left'>postal_code</a>&nbsp;</div><div class='simple-div-table-col right'>".$row['postal_code']."&nbsp;</div>
+							<div class='simple-div-table-col left'>postal_code</a>&nbsp;</div>
+							<div class='simple-div-table-col right'>".$row['postal_code']."&nbsp;</div>
 						</div>
 						<div class='simple-div-table-row'>
-							<div class='simple-div-table-col left'>latitude</a>&nbsp;</div><div class='simple-div-table-col right'>".$row['latitude']."&nbsp;</div>
+							<div class='simple-div-table-col left'>latitude</a>&nbsp;</div>
+							<div class='simple-div-table-col right'>".$row['latitude']."&nbsp;</div>
 						</div>
 						<div class='simple-div-table-row'>
-							<div class='simple-div-table-col left'>longitude</a>&nbsp;</div><div class='simple-div-table-col right'>".$row['longitude']."&nbsp;</div>
+							<div class='simple-div-table-col left'>longitude</a>&nbsp;</div>
+							<div class='simple-div-table-col right'>".$row['longitude']."&nbsp;</div>
 						</div>
 						<div class='simple-div-table-row'>
-							<div class='simple-div-table-col left'>accuracy_radius</a>&nbsp;</div><div class='simple-div-table-col right'>".$row['accuracy_radius'].$km."&nbsp;</div>
+							<div class='simple-div-table-col left'>accuracy_radius</a>&nbsp;</div>
+							<div class='simple-div-table-col right'>".$row['accuracy_radius'].$km."&nbsp;</div>
 						</div>
 						<div class='simple-div-table-row'>
-							<div class='simple-div-table-col left'>locale_code</a>&nbsp;</div><div class='simple-div-table-col right'>".$row['locale_code']."&nbsp;</div>
+							<div class='simple-div-table-col left'>locale_code</a>&nbsp;</div>
+							<div class='simple-div-table-col right'>".$row['locale_code']."&nbsp;</div>
 						</div>
 						<div class='simple-div-table-row'>
-							<div class='simple-div-table-col left'>continent_code</a>&nbsp;</div><div class='simple-div-table-col right'>".$row['continent_code']."&nbsp;</div>
+							<div class='simple-div-table-col left'>continent_code</a>&nbsp;</div>
+							<div class='simple-div-table-col right'>".$row['continent_code']."&nbsp;</div>
 						</div>
 						<div class='simple-div-table-row'>
-							<div class='simple-div-table-col left'>continent_name</a>&nbsp;</div><div class='simple-div-table-col right'>".$row['continent_name']."&nbsp;</div>
+							<div class='simple-div-table-col left'>continent_name</a>&nbsp;</div>
+							<div class='simple-div-table-col right'>".$row['continent_name']."&nbsp;</div>
 						</div>
 						<div class='simple-div-table-row'>
-							<div class='simple-div-table-col left'>country_code</a>&nbsp;</div><div class='simple-div-table-col right'>".$row['country_code']."&nbsp;</div>
+							<div class='simple-div-table-col left'>country_code</a>&nbsp;</div>
+							<div class='simple-div-table-col right'>".$row['country_code']."&nbsp;</div>
 						</div>
 						<div class='simple-div-table-row'>
-							<div class='simple-div-table-col left'>country_name</a>&nbsp;</div><div class='simple-div-table-col right'>".$row['country_name']."&nbsp;</div>
+							<div class='simple-div-table-col left'>country_name</a>&nbsp;</div>
+							<div class='simple-div-table-col right'>".$row['country_name']."&nbsp;</div>
 						</div>
 						<div class='simple-div-table-row'>
-							<div class='simple-div-table-col left'>subdivision_1_iso_code</a>&nbsp;</div><div class='simple-div-table-col right'>".$row['subdivision_1_iso_code']."&nbsp;</div>
+							<div class='simple-div-table-col left'>subdivision_1_iso_code</a>&nbsp;</div>
+							<div class='simple-div-table-col right'>".$row['subdivision_1_iso_code']."&nbsp;</div>
 						</div>
 						<div class='simple-div-table-row'>
-							<div class='simple-div-table-col left'>state_abbr</a>&nbsp;</div><div class='simple-div-table-col right'>".$row['subdivision_1_iso_code']."&nbsp;</div>
+							<div class='simple-div-table-col left'>state_abbr</a>&nbsp;</div>
+							<div class='simple-div-table-col right'>".$row['subdivision_1_iso_code']."&nbsp;</div>
 						</div>
 						<div class='simple-div-table-row'>
-							<div class='simple-div-table-col left'>subdivision_1_name</a>&nbsp;</div><div class='simple-div-table-col right'>".$row['subdivision_1_name']."&nbsp;</div>
+							<div class='simple-div-table-col left'>subdivision_1_name</a>&nbsp;</div>
+							<div class='simple-div-table-col right'>".$row['subdivision_1_name']."&nbsp;</div>
 						</div>
 						<div class='simple-div-table-row'>
-							<div class='simple-div-table-col left'>state_name</a>&nbsp;</div><div class='simple-div-table-col right'>".$row['subdivision_1_name']."&nbsp;</div>
+							<div class='simple-div-table-col left'>state_name</a>&nbsp;</div>
+							<div class='simple-div-table-col right'>".$row['subdivision_1_name']."&nbsp;</div>
 						</div>
 						<div class='simple-div-table-row'>
-							<div class='simple-div-table-col left'>subdivision_2_iso_code</a>&nbsp;</div><div class='simple-div-table-col right'>".$row['subdivision_2_iso_code']."&nbsp;</div>
+							<div class='simple-div-table-col left'>subdivision_2_iso_code</a>&nbsp;</div>
+							<div class='simple-div-table-col right'>".$row['subdivision_2_iso_code']."&nbsp;</div>
 						</div>
 						<div class='simple-div-table-row'>
-							<div class='simple-div-table-col left'>subdivision_2_name</a>&nbsp;</div><div class='simple-div-table-col right'>".$row['subdivision_2_name']."&nbsp;</div>
+							<div class='simple-div-table-col left'>subdivision_2_name</a>&nbsp;</div>
+							<div class='simple-div-table-col right'>".$row['subdivision_2_name']."&nbsp;</div>
 						</div>
 						<div class='simple-div-table-row'>
-							<div class='simple-div-table-col left'>city_name</a>&nbsp;</div><div class='simple-div-table-col right'>".$row['city_name']."&nbsp;</div>
+							<div class='simple-div-table-col left'>city_name</a>&nbsp;</div>
+							<div class='simple-div-table-col right'>".$row['city_name']."&nbsp;</div>
 						</div>
 						<div class='simple-div-table-row'>
-							<div class='simple-div-table-col left'>metro_code</a>&nbsp;</div><div class='simple-div-table-col right'>".$row['metro_code']."&nbsp;</div>
+							<div class='simple-div-table-col left'>metro_code</a>&nbsp;</div>
+							<div class='simple-div-table-col right'>".$row['metro_code']."&nbsp;</div>
 						</div>
 						<div class='simple-div-table-row'>
-							<div class='simple-div-table-col left'>time_zone</a>&nbsp;</div><div class='simple-div-table-col right'>".$row['time_zone']."&nbsp;</div>
+							<div class='simple-div-table-col left'>time_zone</a>&nbsp;</div>
+							<div class='simple-div-table-col right'>".$row['time_zone']."&nbsp;</div>
 						</div>
 						<div class='simple-div-table-row'>
-							<div class='simple-div-table-col left'>is_in_european_union</a>&nbsp;</div><div class='simple-div-table-col right'>".$eu."&nbsp;</div>
+							<div class='simple-div-table-col left'>is_in_european_union</a>&nbsp;</div>
+							<div class='simple-div-table-col right'>".$eu."&nbsp;</div>
 						</div>";
 		}
 
@@ -146,18 +167,69 @@
 			</center>
 		</div>
 
+		// https://stackoverflow.com/questions/50258902/how-to-zoom-area-within-15km-radius-from-the-marker
 		<script src='https://unpkg.com/leaflet@1.6.0/dist/leaflet.js'></script>
 		<script>
-			var element = document.getElementById('map');
-			var map = L.map(element);
-			L.tileLayer('https://{s}.tile.osm.org/{z}/{x}/{y}.png', {
-				attribution: \"&copy; <a href='https://osm.org/copyright'>OpenStreetMap</a> contributors\"
-			}).addTo(map);
-			var target = L.latLng('".$latitude."', '".$longitude."');
-			map.setView(target, 10);
-			L.marker(target).addTo(map); 
+			// Create the map
+			var map = L.map('map').setView([".$latitude.", ".$longitude."], 12);
+
+			// Set up the OSM layer
+			L.tileLayer(
+				'http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+					maxZoom: 18
+				}
+			).addTo(map);
+
+			// add a marker in the given location
+			var lat = ".$latitude.";
+			var lng = ".$longitude.";
+			var marker = L.marker([lat, lng]).addTo(map); // accuracy radius in meters.
+			var radius = ".($accuracy_radius * 1000)."; 
+			var circle = L.circle([lat, lng], radius).addTo(map);
+
+			// add a layer and add points
+			var myLayer = L.geoJson().addTo(map);
+
+			// geojsonFeature
+			var geojsonFeature = {
+				'type': 'IP Address',
+				'properties': {
+					'IP': '".$geoip."',
+					'popupContent': 'Accuracy Radius: ".$accuracy_radius."'
+				},
+				'geometry': {
+					'type': 'Point',
+					'coordinates': [".$latitude.", ".$longitude."]
+				}
+			};
+
+			// put the marker
+			setTimeout(function() {
+				myLayer.addData(geojsonFeature);
+			}, 1000);
+
+			// update the marker
+			setTimeout(function() {
+				// clear layer
+				myLayer.clearLayers(); // inherited from LayerGroup
+				//myLayer.addData(geojsonFeature);
+			}, 3000);
+
+			// put the marker
+			setTimeout(function() {
+				myLayer.addData(geojsonFeature);
+			}, 5000);
+
+			// just fooling around
+			setInterval(function() {
+				lat = lat + ((Math.random() * 1) - 0.25) * 0.001;
+				lng = lng + ((Math.random() * 1) - 0.5) * 0.001;
+				var newLatLng = [lat, lng];
+				marker.setLatLng(newLatLng).update();
+				circle.setLatLng(newLatLng);
+				map.fitBounds(circle.getBounds());
+			}, 200);
 		</script>";
-		
 	}
 
 	echo "
